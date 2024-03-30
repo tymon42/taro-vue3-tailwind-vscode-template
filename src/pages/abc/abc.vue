254e224f7214a5d9a30139e900eeacda8c2cf030
HarryWong <tymon42@outlook.com>
2024-03-30 21:41:47 +0800

    更新 abc.vue 文件中的模板、脚本和样式

--- src/pages/abc/abc.vue
+++ src/pages/abc/abc.vue
@@ -1,21 +1,20 @@
 <template>
-    <view>
-      <Dongdong class="w-10 h-10" />
-    </view>
-  </template>
-  
-  <script setup lang="ts">
-  import { Dongdong } from '@nutui/icons-vue-taro';
-  
-  
-  </script>
-  
-  <style lang="scss">
-  .index {
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-  }
-  </style>
-  
+  <view>
+    <Dongdong class="w-10 h-10" />
+  </view>
+</template>
+
+<script setup lang="ts">
+import { Dongdong } from '@nutui/icons-vue-taro';
+
+
+</script>
+
+<style lang="scss">
+.index {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
